b0516574cdf35be4fd7d204ca5eba9531e93f756
Adriaan Putter <adriaan.putter@gmail.com>
2023-05-13 00:11:00 +1200

    Add dap quality of life

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -13,20 +13,26 @@ lsp.configure('sumneko_lua', {
   }
 })
 
--- local cmp = require('cmp')
--- local cmp_mappings = lsp.defaults.cmp_mappings({
---   ['<C-Space>'] = cmp.mapping.complete(),
---   ['<C-e>'] = cmp.mapping.abort(),
--- })
---
--- local cmp_sources = lsp.defaults.cmp_sources()
---
--- table.insert(cmp_sources, {name = 'nvim_lsp'})
---
--- lsp.setup_nvim_cmp({
---   mapping = cmp_mappings,
---   sources = cmp_sources
--- })
+local cmp = require('cmp')
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.abort(),
+})
 
+-- local cmp_sources = lsp.defaults.cmp_sources()
+
+-- table.insert(cmp_sources, {name = 'nvim_lsp'})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings,
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  lsp.default_keymaps({buffer = bufnr})
+  
+  vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+end)
 
 lsp.setup()
