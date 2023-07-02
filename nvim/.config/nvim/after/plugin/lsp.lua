884d7e51b94a5ecaadd94c2d6a3f7cb1c0e56ce0
Adriaan Putter <adriaan.putter@gmail.com>
2023-07-02 22:30:43 +1200

    Better configure cmp in lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -13,26 +13,23 @@ lsp.configure('sumneko_lua', {
   }
 })
 
-local cmp = require('cmp')
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-})
-
--- local cmp_sources = lsp.defaults.cmp_sources()
-
--- table.insert(cmp_sources, {name = 'nvim_lsp'})
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
-})
-
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
-  lsp.default_keymaps({buffer = bufnr})
-  
+  lsp.default_keymaps({ buffer = bufnr })
+
   vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
+
+local cmp = require('cmp')
+cmp.setup({
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert,preview'
+  },
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete()
+  },
+})
