be8ef2cd43f0ce4bf0e1641427ca6e98b1e9d2e2
Adriaan Putter <adriaan.putter@gmail.com>
2024-03-26 22:48:27 +1300

    Add nvim-nio package

--- nvim/.config/nvim/lua/wap/packer.lua
+++ nvim/.config/nvim/lua/wap/packer.lua
@@ -43,7 +43,7 @@ return require('packer').startup(function(use)
 	use('mfussenegger/nvim-dap')
 	use {
 		'rcarriga/nvim-dap-ui',
-		requires = { 'mfussenegger/nvim-dap' }
+		requires = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' }
 	}
 
 	use 'folke/neodev.nvim'
