68b003aefccdfcde68afdea0ee846641f316d521
Adriaan Putter <adriaan.putter@gmail.com>
2025-07-26 19:24:22 +1200

    Remove shortcut for signature help

--- nvim/.config/nvim/lua/wap/init.lua
+++ nvim/.config/nvim/lua/wap/init.lua
@@ -27,7 +27,6 @@ autocmd("LspAttach", {
 		vim.keymap.set("n", "<F12>", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<C-h>", vim.lsp.buf.signature_help, opts)
 		vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
 		vim.keymap.set({ "n", "x" }, "<F3>", function()
 			require("conform").format({ async = true, lsp_format = "fallback" })
