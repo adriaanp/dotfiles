d1173a2cf7f8de736751e2a1625cdf6a5882fb93
Adriaan Putter <adriaan.putter@gmail.com>
2025-08-13 13:21:52 +1200

    Configure conform for formatting

--- nvim/.config/nvim/lua/wap/lazy/lsp.lua
+++ nvim/.config/nvim/lua/wap/lazy/lsp.lua
@@ -1,120 +1,136 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"hrsh7th/cmp-nvim-lsp",
-			},
-			{
-				"stevearc/conform.nvim",
-				opts = {
-					formatters_by_ft = {
-						lua = { "stylua" },
-						cs = { "cshapier" },
-					},
-				},
-			},
-			{
-				"williamboman/mason-lspconfig.nvim",
-				dependencies = {
-					"williamboman/mason.nvim",
-				},
-			},
-			{
-				"hrsh7th/nvim-cmp",
-				dependencies = {
-					"L3MON4D3/LuaSnip",
-					dependencies = {
-						"saadparwaiz1/cmp_luasnip",
-						"rafamadriz/friendly-snippets",
-					},
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "hrsh7th/cmp-nvim-lsp",
+      },
+      {
+        "stevearc/conform.nvim",
+        opts = {
+          formatters_by_ft = {
+            lua = { "stylua" },
+            cs = { "cshapier" },
+            javascript = { "prettierd", "prettier" },
+            markdown = { "prettierd", "prettier" },
+          },
+          default_format_opts = {
+            lsp_format = "fallback",
+          },
+        },
+        keys = {
+          {
+            "<leader>f",
+            function()
+              require("conform").format({ async = true })
+            end,
+            mode = "",
+            desc = "Format buffer"
+          },
+        },
+      },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+          "williamboman/mason.nvim",
+        },
+      },
+      {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+          "L3MON4D3/LuaSnip",
+          dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+          },
 
-					config = function()
-						local ls = require("luasnip")
+          config = function()
+            local ls = require("luasnip")
 
-						vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
-						vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
-						vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+            vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+            vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
 
-						vim.keymap.set({ "i", "s" }, "<C-E>", function() if ls.choice_active() then ls.change_choice(1) end end, { silent = true })
-					end,
-				},
+            vim.keymap.set({ "i", "s" }, "<C-E>", function() if ls.choice_active() then ls.change_choice(1) end end,
+              { silent = true })
+          end,
+        },
 
-				config = function()
-					local cmp = require("cmp")
-					require("luasnip.loaders.from_vscode").lazy_load()
+        config = function()
+          local cmp = require("cmp")
+          require("luasnip.loaders.from_vscode").lazy_load()
 
-					cmp.setup({
-						preselect = cmp.PreselectMode.None,
-						completion = {
-							completeopt = "menu,menuone,noinsert,noselect,preview",
-						},
-						snippet = {
-							expand = function(args)
-								require("luasnip").lsp_expand(args.body)
-							end,
-						},
-						window = {
-							completion = cmp.config.window.bordered(),
-							documentation = cmp.config.window.bordered(),
-						},
-						mapping = cmp.mapping.preset.insert({
-							["<C-Space>"] = cmp.mapping.complete(),
-							["<CR>"] = cmp.mapping.confirm({ select = true }),
-						}),
-						sources = cmp.config.sources({
-							{ name = "nvim_lsp" },
-							{ name = "luasnip" },
-						}, {
-							{ name = "buffer" },
-						}),
-					})
-				end,
-			},
-		},
+          cmp.setup({
+            preselect = cmp.PreselectMode.None,
+            completion = {
+              completeopt = "menu,menuone,noinsert,noselect,preview",
+            },
+            snippet = {
+              expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+              end,
+            },
+            window = {
+              completion = cmp.config.window.bordered(),
+              documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+              ["<C-Space>"] = cmp.mapping.complete(),
+              ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            }),
+            sources = cmp.config.sources({
+              { name = "nvim_lsp" },
+              { name = "luasnip" },
+            }, {
+              { name = "buffer" },
+            }),
+          })
+        end,
+      },
+    },
 
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-				},
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+        },
 
-				handlers = {
-					function(server_name)
-						lspconfig[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
+        handlers = {
+          function(server_name)
+            lspconfig[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
 
-					lua_ls = function()
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									runtime = { version = "LuaJIT" },
-									diagnostics = {
-										globals = { "vim" },
-									},
-									workspace = {
-										library = {
-											vim.env.VIMRUNTIME,
-										},
-									},
-								},
-							},
-						})
-					end,
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ui = { border = "single" },
-		},
-	},
+          lua_ls = function()
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  runtime = { version = "LuaJIT" },
+                  diagnostics = {
+                    globals = { "vim" },
+                  },
+                  workspace = {
+                    library = {
+                      vim.env.VIMRUNTIME,
+                    },
+                  },
+                },
+              },
+            })
+          end,
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = { border = "single" },
+    },
+  },
 }
