5391fecb317b7722845b77de7e2e62d2a969e877
Adriaan Putter <adriaan.putter@gmail.com>
2025-08-24 21:44:23 +1200

    Update keymaps and telescope keymaps

--- nvim/.config/nvim/lua/wap/keymaps.lua
+++ nvim/.config/nvim/lua/wap/keymaps.lua
@@ -1,29 +1,30 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-keymap({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-keymap('n', '<leader>e', vim.cmd.Ex)
-keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
-keymap('v', 'J', ":m '>+1<CR>gv=gv", opts)
-keymap('v', 'K', ":m '<-2<CR>gv=gv", opts)
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
 
-keymap('n', '<leader>y', '"+y', opts)
-keymap('n', '<leader>Y', '"+Y', opts)
+vim.keymap.set('v', 'p', '"_dP')
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_dP')
 
-keymap('v', 'p', '"_dP', opts)
-keymap({ 'n', 'v' }, '<leader>d', '"_dP')
+vim.keymap.set("n", "<esc>", function()
+  vim.cmd.noh()
+  return "<esc>"
+end)
 
-keymap('n', '<leader>f', vim.lsp.buf.format)
-
-keymap('n', '<leader>x', vim.cmd.bdelete, opts)
-keymap('n', '<leader>xx', function() vim.cmd('%bd') end, opts)
+vim.keymap.set("n", "<leader>xx", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
